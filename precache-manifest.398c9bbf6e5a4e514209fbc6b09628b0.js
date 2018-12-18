83628821c75b490bc89cb542e442c2a059f73773
carebear <carie@gawkbox.com>
2018-12-18 12:56:26 -0800

    Updates

---
+++ precache-manifest.398c9bbf6e5a4e514209fbc6b09628b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17321a1ec1a4025e252f",
+    "url": "/protoman/static/js/runtime~main.17321a1e.js"
+  },
+  {
+    "revision": "0de73de8f6d1bee4ad37",
+    "url": "/protoman/static/js/main.0de73de8.chunk.js"
+  },
+  {
+    "revision": "05f08b140d37d92993bb",
+    "url": "/protoman/static/js/1.05f08b14.chunk.js"
+  },
+  {
+    "revision": "0de73de8f6d1bee4ad37",
+    "url": "/protoman/static/css/main.dba4c2ca.chunk.css"
+  },
+  {
+    "revision": "518357265bd49f7ffa374aa0b7062d97",
+    "url": "/protoman/index.html"
+  }
+];
